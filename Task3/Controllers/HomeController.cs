1d120e1f23f9e58a830005afef5bf5cd1a6a8d67
Andrew Nazarov <xiss@users.noreply.github.com>
2022-07-26 23:58:33 +0300

    Add project files.

---
+++ Task3/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Page1()
+        {
+            return View();
+        }
+        public IActionResult Page2()
+        {
+            return View();
+        }
+        public IActionResult Page3()
+        {
+            return View();
+        }
+        public IActionResult Page4()
+        {
+            return View();
+        }
+        public IActionResult Page5()
+        {
+            return View();
+        }
+    }
+}
